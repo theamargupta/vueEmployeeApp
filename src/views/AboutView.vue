c698933a7015540da4b83e263f1656beb4605ab2
theamargupta <theamargupta.tech@gmail.com>
2023-04-14 05:49:24 +0530

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { reactive } from 'vue'
+
+const state = reactive({ count: 0 })
+
+function increment() {
+  state.count++
+}
+</script>
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button @click="increment">{{ count }}</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
